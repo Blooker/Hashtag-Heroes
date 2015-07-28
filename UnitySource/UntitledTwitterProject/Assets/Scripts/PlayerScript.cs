a627ec762469e4dcbfaa512f2e4947a337caea6e
Blooker <evangriffiths@hotmail.co.uk>
2015-07-28 10:33:18 +0100

    Changed gameplay from 2D platformer to Flappy Bird style dodge em up

---
+++ UnitySource/UntitledTwitterProject/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	ParticleSystem flameEffect;
+	ParticleSystem smokeEffect;
+	bool keyPress = false;
+
+	// Use this for initialization
+	void Start () {
+		flameEffect = GameObject.Find ("FlameParticles").GetComponent<ParticleSystem> ();
+		smokeEffect = GameObject.Find ("SmokeParticles").GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		GetComponent<Rigidbody2D> ().velocity *= 0.97f;
+		if (Input.GetKey (KeyCode.W)) {
+			flameEffect.Emit(2);
+			smokeEffect.Emit(2);
+			GetComponent<Rigidbody2D> ().AddForce (transform.up * 20f);
+		} else if (Input.GetKey (KeyCode.S)) {
+			keyPress = true;
+			GetComponent<Rigidbody2D> ().AddForce (transform.up * -20f);
+		} else {
+			flameEffect.Emit(2);
+			smokeEffect.Emit(2);
+		}
+	}
+}
--- UnitySource/UntitledTwitterProject/Assets/Scripts/Twitter.cs
+++ UnitySource/UntitledTwitterProject/Assets/Scripts/Twitter.cs
@@ -5,11 +5,15 @@ public class Twitter : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		GetComponent<Rigidbody2D> ().velocity *= 0.97f;
+		if (Input.GetKey (KeyCode.W)) {
+			Debug.Log ("YEE");
+		}
+		GetComponent<Rigidbody2D> ().AddForce(transform.up * 10f);
 	}
 }
